875b3220488b79cc5eb429fa0e0ed9c6b7d84d4d
Vishal Gaurav <lemeraemail@gmail.com>
2022-04-03 23:03:21 +0530

    feat: add landing page

--- src/routes/landing-page/LandingPage.jsx
+++ src/routes/landing-page/LandingPage.jsx
@@ -1,9 +1,32 @@
 import "./landing-page.css";
-
+import "../../style.css";
+import note_svg from "../../assets/note_svg.svg";
 
 const LandingPage = () => {
   return (
-    <h1>This is landing Page</h1>
+    <div className="container">
+      <div className="landing-page-container">
+        <div className="landing-page-content-container">
+          <div className="landing-logo-container">
+            <span className="landing-logo primary">Note</span>
+            <span className="landing-logo secondary">Stalk</span>
+          </div>
+          <p className="xx-large bold">Meet your modern</p>
+          <p className="primary xx-large">Note Taking App</p>
+          <p className="large landing-description">
+            Mange your daily tasks and workflow in a modern way and boost your
+            efficiency without any efforts.
+          </p>
+          <button className="landing-cta">Join Now</button>
+          <button className="btn btn-link landing-signup">
+            Already have an account?
+          </button>
+        </div>
+        <div className="landing-page-img-container">
+          <img className="landing-page-img" src={note_svg} />
+        </div>
+      </div>
+    </div>
   );
 };
 export { LandingPage };
